85f8e55381032abd9fd2680be140b2c84cadd1ea
Peter Polidoro <peterpolidoro@gmail.com>
2020-09-04 15:01:09 -0400

    Update to work with ArduinoJson 6

--- src/EncoderInterfaceMagnetic/EncoderInterfaceMagnetic.cpp
+++ src/EncoderInterfaceMagnetic/EncoderInterfaceMagnetic.cpp
@@ -174,8 +174,8 @@ size_t EncoderInterfaceMagnetic::getSampleCountMax()
 // floating-point number (float, double)
 // bool
 // const char *
-// ArduinoJson::JsonArray *
-// ArduinoJson::JsonObject *
+// ArduinoJson::JsonArray
+// ArduinoJson::JsonObject
 // const ConstantString *
 //
 // For more info read about ArduinoJson parsing https://github.com/janelia-arduino/ArduinoJson
