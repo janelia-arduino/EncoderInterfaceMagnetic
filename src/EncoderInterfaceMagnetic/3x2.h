726347d86a9db0b20acc5f49c55fc27cfa9a3254
Peter Polidoro <peterpolidoro@gmail.com>
2021-09-16 13:03:22 -0400

    Update email address

--- src/EncoderInterfaceMagnetic/3x2.h
+++ src/EncoderInterfaceMagnetic/3x2.h
@@ -3,7 +3,7 @@
 //
 //
 // Authors:
-// Peter Polidoro peterpolidoro@gmail.com
+// Peter Polidoro peter@polidoro.io
 // ----------------------------------------------------------------------------
 #ifndef ENCODER_INTERFACE_MAGNETIC_3X2_CONSTANTS_H
 #define ENCODER_INTERFACE_MAGNETIC_3X2_CONSTANTS_H
